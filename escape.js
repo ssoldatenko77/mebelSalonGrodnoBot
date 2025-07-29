a4d1bd5741bd58118d7d4d9b1343ec85a747d933
Sergey Soldatenko <ssoldatenko77@gmail.com>
2025-07-29 12:03:11 +0300

    Обновил escape.js, escaped.txt и .gitignore

--- escape.js
+++ escape.js
@@ -1,5 +1,11 @@
 const fs = require('fs');
 
-const raw = fs.readFileSync('./service-account.json', 'utf8');
-const escaped = raw.replace(/\n/g, '\\n');
-console.log(escaped);
+const filePath = 'service-account.json';
+const outputPath = 'service-account.b64';
+
+const jsonBuffer = fs.readFileSync(filePath);
+const base64String = jsonBuffer.toString('base64');
+
+fs.writeFileSync(outputPath, base64String);
+
+console.log('Base64 записан в', outputPath);

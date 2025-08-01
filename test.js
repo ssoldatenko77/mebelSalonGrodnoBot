53732bf0e0644ae79d47f1ac7173a375e28c490a
Sergey Soldatenko <ssoldatenko77@gmail.com>
2025-08-01 12:16:14 +0300

    chore: удалён test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-console.log('GOOGLE_SERVICE_ACCOUNT:', process.env.GOOGLE_SERVICE_ACCOUNT);
-
-try {
-  const creds = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT);
-  console.log('Parsed private_key:', creds.private_key.substring(0, 50)); // первые 50 символов
-} catch (err) {
-  console.error('JSON parse error:', err.message);
-}
